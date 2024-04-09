1d95e2995055ad28ad6bac5666109861a9727341
alu10183093 <magoga2896@gmail.com>
2024-04-09 19:55:52 +0200

    db connection

---
+++ Jugador.java
@@ -0,0 +1,41 @@
+public class Jugador {
+    public int codPais;
+    public String nombrePais;
+    public String nombreJugador;
+    public int añoNacimiento;
+    public float altura;
+    public String club;
+
+    public Jugador(int codPais, String nombrePais, String nombreJugador, int añoNacimiento, float altura, String club){
+        this.codPais = codPais;
+        this.nombrePais = nombrePais;
+        this.nombreJugador = nombreJugador;
+        this.añoNacimiento = añoNacimiento;
+        this.altura = altura;
+        this.club = club;
+    }
+
+    public int getCodPais() {
+        return codPais;
+    }
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public int getAñoNacimiento() {
+        return añoNacimiento;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public String getClub() {
+        return club;
+    }
+}
--- Test.java
+++ Test.java
@@ -1,4 +1,16 @@
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Test {
+    public static void main(String[] args) throws SQLException {
+        File fichero = new File("./recursos/seleccion_EEUU.dat");
+        cargarFichero(fichero);
+    }
     static java.sql.Connection con = DatabaseConnection.getInstance().getConnection();
 
     public Test() {
@@ -29,7 +41,36 @@ public class Test {
 
     }
 
-    public void cargarFichero() {
-        //d
+    public static void cargarFichero(File fichero) throws SQLException {
+        Statement st = con.createStatement();
+        ArrayList<Jugador> jugadores = arrayJugadores(fichero);
+
+        String sqlPais = "INSERT INTO pais VALUES (" + jugadores.get(0).getCodPais() + ",'" + jugadores.get(0).getNombrePais() + "');";
+        st.executeUpdate(sqlPais);
+
+        for (int i = 0; i < jugadores.size(); i++) {
+            String sqlJugador = "INSERT INTO jugador(nombre_jugador, año_nacimiento, altura, club, id_pais) VALUES ('" +
+                    jugadores.get(i).getNombreJugador() + "'," + jugadores.get(i).getAñoNacimiento() + ","
+                    + jugadores.get(i).getAltura() + ",'" + jugadores.get(i).getClub() + "',"
+                    + jugadores.get(i).getCodPais() + ");";
+            st.executeUpdate(sqlJugador);
+        }
+        st.close();
+    }
+
+    public static ArrayList arrayJugadores(File f) {
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+
+        try {
+            DataInputStream f_in = new DataInputStream((new FileInputStream(f)));
+            while(f_in.available() > 0) {
+                jugadores.add(new Jugador(f_in.readInt(), f_in.readUTF(), f_in.readUTF(), f_in.readInt(), f_in.readFloat(), f_in.readUTF()));
+            }
+            f_in.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return jugadores;
     }
 }
